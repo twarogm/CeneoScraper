cef1d147ea0b2addf5160b28481c86b689ad9917
Marcin Twaróg <56258509+twarogm@users.noreply.github.com>
2020-04-08 14:13:50 +0200

    MT 2020-04-08

---
+++ analyzer.py
@@ -0,0 +1,15 @@
+#import bibliotek
+import os
+import pandas as pd
+
+#wyswietlenie zawartości katalogu z opiniami
+print(*os.listdir('./opinions_json'))
+
+#wczytanie identyfiaktora produktu, którego opinie będą analizowane
+product_id = input('Podaj kod produktu: ')
+
+#wczytanie do ramki danych opinii z pliku
+opinions = pd.read_json('./opinions_json/'+product_id+'.json')
+opinions = opinions.set_index('opinion_id')
+
+print(opinions)
--- scraper.py
+++ scraper.py
@@ -26,6 +26,13 @@ tags = {
     "purchased":['div', 'product-review-pz', 'em']
 }
 
+#funkcja do usuwania znaków formatujących
+def remove_whitespaces(string):
+    try:
+        return string.replace('\n', ', ').replace('\r', ', ')
+    except AttributeError:
+        pass
+
 #adres URL przykładowej strony z opiniami
 url_prefix = 'https://www.ceneo.pl'
 product_id = input('Podaj kod produktu: ')
@@ -49,7 +56,13 @@ while url:
                         for key, args in tags.items()}
 
         features['purchased'] = (features['purchased'] == 'Opinia potwierdzona zakupem')
-        features["opinion_id"] = opinion['data-entry-id']
+        features["opinion_id"] = int(opinion['data-entry-id'])
+        features['useful'] = int(features['useful'])
+        features['useless'] = int(features['useless'])
+        features['content'] = remove_whitespaces(features['content'])
+        features['pros'] = remove_whitespaces(features['pros'])
+        features['cons'] = remove_whitespaces(features['cons'])
+        
         dates = opinion.find('span', 'review-time').find_all('time')
         features['review_date'] = dates.pop(0)['datetime']
         try:
